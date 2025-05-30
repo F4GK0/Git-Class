d15d47bac83f11a8869835f258b0aeb04573069a
Edgar Castro <castromendezedgarleonel@gmail.com>
2025-05-29 18:31:12 -0600

    Te odio

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("Hola Mundo")
 console.log("Hola Mundo 2")
+
+console.log("Trabajando duro")
+console.log("Agregando lo faltante")
+
+DETESTO TODO 
+BYE 
+RENUNCIO
+
+GRACIAS POR EL INTENTO POR EL DINERO 
